e3bf3117ab905394bbf902f5f6daf2a22e07da26
kazinov <kazinov.dima@gmail.com>
2016-01-30 03:25:09 +0700

    get fix

--- js/bundle.js
+++ js/bundle.js
@@ -5247,9 +5247,6 @@ angular.mock.animate = angular.module('ngAnimateMock', ['ng'])
         on: $delegate.on,
         off: $delegate.off,
         pin: $delegate.pin,
-        get: reflows() {
-          return $$forceReflow.totalReflows;
-        },
         enabled: $delegate.enabled,
         /**
          * @ngdoc method
